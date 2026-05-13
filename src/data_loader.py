2723242a654740a04679bc3cdeb4532877559697
paul duran roldan <paul.duran.roldan@gmail.com>
2026-05-12 23:00:30 -0600

    realice la funcion de rellenar los espacios vacios, sacar la mediana y rellenar los espacios vacios con lo anterior. y finalice con el df_limpio para su uso completo del dataset previamente trabajado

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,2 +1,15 @@
-def load_data(): 
-	pass
+import pandas as pd
+import numpy as np
+import yaml
+def load_and_clean_data(input_path):
+		df = pd.read_csv(input_path)
+		df['TotalCharges']= pd.to_numeric(df['TotalCharges'], errors='coerce')
+		median_value = df['TotalCharges'].median()
+		df['TotalCharges'] = df['TotalCharges'].fillna(median_value)
+		return df 
+
+
+#def load_data(): 
+#	pass
+
+
