1c8a7f9805cec50b91330f2e8dec91ef5ddfb736
paul duran roldan <paul.duran.roldan@gmail.com>
2026-05-12 23:24:04 -0600

    Fin , agregue la ruta directa para verificar el dataset limpio en main y arregle errores

--- src/main.py
+++ src/main.py
@@ -1,12 +1,7 @@
 from data_loader import load_and_clean_data
-
+#Este bloque te entrega el dataset listo y se guarda en data/processed/data_cleaned.csv. :)) P
 if __name__ == "__main__":
     ruta = "data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv"
-    print("Iniciando el pipeline...")
-    
-    # Llamamos a la función que escribiste en el otro archivo
     df = load_and_clean_data(ruta)
-    
-    # Guardamos
     df.to_csv("data/processed/data_cleaned.csv", index=False)
-    print("¡Proceso terminado desde main.py!")
+    print("¡Proceso terminado!")
