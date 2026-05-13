6fb8b340b295b7374ae0b2d1525e37e46f360d20
paul duran roldan <paul.duran.roldan@gmail.com>
2026-05-12 23:45:34 -0600

    agregue a la funcion el eliminar el customerID, codificar el gender partner y churn a 0 y 1, y en el main agregue el dividir el dataset y entrenarlo

--- src/data_loader.py
+++ src/data_loader.py
@@ -7,6 +7,21 @@ def load_and_clean_data(input_path):
 		df['TotalCharges']= pd.to_numeric(df['TotalCharges'], errors='coerce')
 		median_value = df['TotalCharges'].median()
 		df['TotalCharges'] = df['TotalCharges'].fillna(median_value)
+		
+
+		if 'customerID' in df.columns:
+			df = df.drop(columns=['customerID'])
+
+		# Gender: Female=0, Male=1 
+		# Partner/Churn: No=0, Yes=1
+
+		binary_map={'Yes':1, 'No':0}
+		gender_map={'Female':0, 'Male':1}
+
+		df['gender'] = df['gender'].map(gender_map)
+		df['Partner'] = df['Partner'].map(binary_map)
+		df['Churn'] = df['Churn'].map(binary_map)
+
 		return df 
 
 
--- src/main.py
+++ src/main.py
@@ -1,7 +1,12 @@
 from data_loader import load_and_clean_data
+from sklearn.model_selection import train_test_split
 #Este bloque te entrega el dataset listo y se guarda en data/processed/data_cleaned.csv. :)) P
 if __name__ == "__main__":
     ruta = "data/raw/WA_Fn-UseC_-Telco-Customer-Churn.csv"
     df = load_and_clean_data(ruta)
-    df.to_csv("data/processed/data_cleaned.csv", index=False)
-    print("¡Proceso terminado!")
+    
+    train, test = train_test_split(df,test_size=0.2, random_state=42)
+    test.to_csv("data/processed/data_cleaned.csv", index=False)
+    train.to_csv("data/processed/train.csv", index=False)
+
+    print(f"¡Éxito! Creados train.csv ({len(train)} filas) y test.csv ({len(test)} filas)")
